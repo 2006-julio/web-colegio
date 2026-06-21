2c1271775c0dc3c018bc04b5235bba82e6202776
2006-julio <150759728+2006-julio@users.noreply.github.com>
2026-06-21 05:18:58 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  site: 'https://2006-julio.github.io',
+  base: '/web-colegio',
+
   devToolbar: {
     enabled: false,
   },
